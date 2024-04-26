7c60e95505a03633f62c5c1a652d36d85a301ea1
Ms Chen <150866920+mschen-sta@users.noreply.github.com>
2024-04-26 16:05:13 -0400

    Initial commit

---
+++ Encryption.java
@@ -0,0 +1,11 @@
+class Encryption extends ConsoleProgram {
+
+  /**
+  * A program the encrypts a message inputted by the user
+  * @author: 
+  */
+  
+  public void run() {
+
+  }
+}
---
+++ EncryptionSolution.java
@@ -0,0 +1,11 @@
+class EncryptionSolution extends ConsoleProgram {
+
+  /**
+  * Description
+  * @author:
+  */
+  
+  public void run() {
+    //coming soon
+  }
+}

6ab4ce97b258a43989eecf2442c4890effeaa67e
penre <Tianyou.zhang25@ycdsbk12.ca>
2024-04-29 18:32:59 -0400

    ez as heck

--- Encryption.java
+++ Encryption.java
@@ -2,11 +2,37 @@ class Encryption extends ConsoleProgram {
 
   /**
   * A program the encrypts a message inputted by the user
-  * @author: 
+  * @author: peter zhang
   */
   
   public void run() {
+    int shiftAmount;
+    String encryptMessage;
+    String strMessage;
+
+    System.out.println("**** ENCRYPTING PROGRAM *****");
+    strMessage = readLine("What is your message? ");
+    shiftAmount = readInt("What is your shift amount? ");
+
+    encryptMessage = encryptMessage(strMessage , shiftAmount);
+
+    System.out.println("The encrypted message is: " + encryptMessage);
+
     
+  }
+  /**
+   * 
+   * @param message
+   * @param shift
+   */
+  public String encryptMessage(String message, int shift) {
+
+    String result = "";
+
+    for (int i = 0; i < message.length(); i++){
+      result += (char)( ' ' + ((message.charAt(i) - ' ' + shift) % 95));
+    }
+    return result;
   }
 
 }
